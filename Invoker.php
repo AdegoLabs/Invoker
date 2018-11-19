1d8854dcd11bc07cb2da05de3692c328bb486471
AdegoLabs <alphagoblin4@gmail.com>
2018-11-19 07:04:21 +0200

    Update Invoker.php

--- Invoker.php
+++ Invoker.php
@@ -1,16 +1,63 @@
 <?php
-class Invoker extends StdClass {
-	public __call($key, $params) {
-		if(!isset($this->{$key})) return function() use (&key, $params) {
-			$params = implode(',', $params);
+class Invoker extends StdClass{
+    private $_obj = null;
 
-			if (is_callable($key)) throw new Exception ('Undefined ' . get_class($this) . '->' . $key . '(' . $params . ')' ,'error');
-			else return call_user_func($key, $params);
-		};
+    public function __clone() {
+    	return clone Invoker($this->getObj);
+    }
 
-		$container = $this->{$key};
+    private static $_instance;
 
-		return call_user_func_array($container, $params);
-	}
+    public static function instance(){
+
+      if(!isset(self::$_instance)) {
+        self::$_instance = new MyGlobals();
+      }
+      return self::$_instance;
+    }
+
+    public function __construct($obj = null) {
+    	$this->setObj($obj);
+    }
+
+    public function getObj() {
+    	if (!is_null($this->_obj))
+    		return $this->_obj;
+    }
+
+    public function setObj($obj = null) {
+    	if (!is_null($obj))
+    		$this->_obj = $obj;
+    }
+
+    public function __call($method, $args = array()) {
+    	$obj = $this->getObj();
+    	
+    	try {
+    		global $$obj;
+
+    		return call_user_func_array(
+	            array($$obj, $method),
+	            $args
+	        );
+    	} catch (Exception $e) {
+    		throw new Exception('Caught exception: ',  $e->getMessage(), "\n");
+		}
+    }
+
+    public function __invoker($method, $args = array()) {
+
+    	if (func_num_args() > 1 ) {
+
+    		return call_user_func_array(
+            	array(
+            		$this, $method
+            	),
+            	$args
+    		);
+    	} else {
+    		$this->setObj($method);
+    	}
+    }
 }
 ?>
